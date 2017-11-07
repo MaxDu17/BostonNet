40a025550a6a0b929e64360130ac1653c5fa2e69
Max Du <maxdubusiness@gmail.com>
2017-11-07 15:34:20 -0500

    added predict

--- Train.py
+++ Train.py
@@ -40,3 +40,7 @@ loss_score = ev["loss"]
 print("Loss: {0:f}".format(loss_score))
 
 
+y = regressor.predict(
+    input_fn=get_input_fn(prediction_set, num_epochs=1, shuffle=False))
+predictions = list(p["predictions"] for p in intertools.islice(y,6))
+print("Predictions: {}".format(str(predictions)))
